38dca2cb229479d6967f07c395cf0a4bfd23c71f
Lucas Rangel <Lucas.Rangel@outlook.com>
2019-07-16 21:04:55 -0300

    Criada classe Piece e Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
+import boardGame.Board;
 import boardGame.BoardException;
 import boardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) throws BoardException {
-		Position pos = new Position(15, 25);
-		System.out.println(pos);
+		
+		Board board = new Board(8, 8);
 
 	}
 
